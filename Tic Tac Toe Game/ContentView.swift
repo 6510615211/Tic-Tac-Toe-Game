f4ff97b4d1859840e5a621e9a4dc4931654bc217
ivy <6510615211@student.tu.ac.th>
2025-02-26 16:11:53 +0700

    Initial Commit

---
+++ Tic Tac Toe Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe Game
+//
+//  Created by นางสาวพรนัชชา ประทีปสังคม on 26/2/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tic Tac Toe Game/Tic_Tac_Toe_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_GameApp.swift
+//  Tic Tac Toe Game
+//
+//  Created by นางสาวพรนัชชา ประทีปสังคม on 26/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
